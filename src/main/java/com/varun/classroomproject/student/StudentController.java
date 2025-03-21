3b2a494d82dde7ea776b097e81275863a99ab101
raagavarsha <160828309+raagavarsha@users.noreply.github.com>
2025-03-21 12:41:26 +0530

    StudentController.java

--- src/main/java/com/varun/classroomproject/student/StudentController.java
+++ src/main/java/com/varun/classroomproject/student/StudentController.java
@@ -1,4 +1,4 @@
-package com.varun.classroomproject.student;
+package com.varsha.classroomproject.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
